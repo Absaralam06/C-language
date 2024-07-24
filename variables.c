cb62fd2502cc1e52ba1f7f25393ec28b0b12effc
Absaralam06 <absar.06alam@gmail.com>
2024-07-24 17:03:09 +0530

    Add files via upload

---
+++ variables.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 5;
+    float b = 3.4;
+    char c = 'f';
+    int e = 6*6; 
+    float g = 9.8 + 0.2; 
+    printf("the value of a is %d \n", a);
+    printf("the value of b is %f \n", b);
+    printf("the value of c is %c \n", c); 
+    printf("the value of e is %d \n", e);
+    printf("the value of g is %f \n", g);
+    
+    return 0;
+}
