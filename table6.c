2dc69c1510744b30704167806a70b00909dd6ee1
Absaralam06 <absar.06alam@gmail.com>
2024-07-25 12:23:09 +0530

    Add files via upload

---
+++ table6.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    printf("6 * 1 = 6\n");
+    printf("6 * 2 = 12\n");
+    printf("6 * 3 = 18\n");
+    printf("6 * 4 = 24\n");
+    printf("6 * 5 = 30\n");
+    printf("6 * 6 = 36\n");
+    printf("6 * 7 = 42\n");
+    printf("6 * 8 = 48\n");
+    printf("6 * 9 = 54\n");
+    printf("6 * 10 = 60\n");
+
+    return 0;
+}
