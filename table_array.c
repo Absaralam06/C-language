e1636117154b6f6bce8b02df82c9ca10cf3e4941
Absaralam06 <absar.06alam@gmail.com>
2024-08-02 21:18:20 +0530

    Add files via upload

---
+++ table_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int mul[10];
+    int n;
+    printf("Enter the number you want write table\n");
+    scanf("%d", &n);
+    for (int i = 0; i < 10; i++)
+    {
+        mul[i]= n*(i+1);
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d X %d = %d\n", n, i+1, mul[i]);
+    }
+    return 0;
+}
